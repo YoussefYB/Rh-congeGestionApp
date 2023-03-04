13fc4d0092af070f5b405a4e5dfc00496241eadb
YoussefYB <94171993+YoussefYB@users.noreply.github.com>
2023-03-04 21:21:55 +0100

    Add files via upload

--- util.dart
+++ util.dart
@@ -1,19 +1,19 @@
 /*
- * MIT License with Additional Conditions
- *
- * Copyright (c) 2023.   Youssef Belahmar
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- * 1. Attribution: Any use of this software must include attribution to the original author(s) and copyright holder(s) in all copies or substantial portions of the software.
- * 2. Non-liability: The software is provided "as is" without warranty of any kind, either express or implied, including but not limited to the warranties of merchantability, fitness for a particular purpose and non infringement. In no event shall the authors or copyright holders be liable for any claim, damages or other liability, whether in an action of contract, tort or otherwise, arising from, out of or in connection with the software or the use or other dealings in the software.
- * 3. Non-commercial use: The software may not be used for commercial purposes, including without limitation the sale of the software, its derivatives, or its components.
- *
- *
- */
+  * MIT License with Additional Conditions
+  *
+  * Copyright (c) 2023.   Youssef Belahmar
+  * Permission is hereby granted, free of charge, to any person obtaining a copy
+  * of this software and associated documentation files (the "Software"), to deal
+  * in the Software without restriction, including without limitation the rights
+  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+  * copies of the Software, and to permit persons to whom the Software is
+  * furnished to do so, subject to the following conditions:
+  * 1. Attribution: Any use of this software must include attribution to the original author(s) and copyright holder(s) in all copies or substantial portions of the software.
+  * 2. Non-liability: The software is provided "as is" without warranty of any kind, either express or implied, including but not limited to the warranties of merchantability, fitness for a particular purpose and non infringement. In no event shall the authors or copyright holders be liable for any claim, damages or other liability, whether in an action of contract, tort or otherwise, arising from, out of or in connection with the software or the use or other dealings in the software.
+  * 3. Non-commercial use: The software may not be used for commercial purposes, including without limitation the sale of the software, its derivatives, or its components.
+  *
+  *
+  */
 
 import 'dart:io';
 import 'dart:typed_data';
@@ -48,51 +48,17 @@ async{
   final formattedDateNow = '${now.day}/${now.month}/${now.year}';
 
   // Chargement de l'image à partir du dossier assets
-  final Uint8List imageDataMoroccoLogo = (await rootBundle.load('assets/moroccologo.png')).buffer.asUint8List();
-  //final Uint8List imageDataMinisterLogo = (await rootBundle.load('assets/ministeresantelogo.jpg')).buffer.asUint8List();
-  //final Uint8List imageDataDelegationLogo = (await rootBundle.load('assets/delegationPrefectorial.jpg')).buffer.asUint8List();
-  //final Uint8List imageDataEntet = (await rootBundle.load('assets/entet.jpg')).buffer.asUint8List();
+  final Uint8List image = (await rootBundle.load('assets/image')).buffer.asUint8List();
 
   doc.addPage(
       pw.MultiPage(
           pageTheme: pageTheme,
           header: (final context) =>
-            pw.Align(
-              alignment: pw.Alignment.center,
-              child: pw.Image(pw.MemoryImage(imageDataMoroccoLogo), width: 470),
-            ),
-
-          /*pw.Row(
-
-            mainAxisAlignment: pw.MainAxisAlignment.spaceBetween,
-            children: [
-
-              pw.Expanded(
-                child:pw.Column(
-              children: [
-                pw.Image(pw.MemoryImage(imageDataDelegationLogo), width: 180),
-                pw.Text(
-                      '\nDS N°       du $formattedDateNow',
-                  style: const pw.TextStyle(
-                    fontSize: 16,
-                  ),
-                ),
-              ]
-          )
+              pw.Align(
+                alignment: pw.Alignment.center,
+                child: pw.Image(pw.MemoryImage(image), width: 470),
               ),
-              pw.Image(pw.MemoryImage(imageDataMoroccoLogo), width: 100),
-              pw.Image(pw.MemoryImage(imageDataMinisterLogo), width: 200),
-              /*pw.Expanded(
-                child: pw.Text(
-                  'المملكة المغربية',
-                  textAlign: pw.TextAlign.right,
-                  style: const pw.TextStyle(
-                    fontSize: 16,
-                  ),
-                ),
-              ),*/
-            ],
-          ),*/
+
 
 
           build: (final context) =>[
@@ -117,21 +83,9 @@ async{
                 ),
               ),
             ),
-              pw.Text(
-                'Le Délégué du Ministère de la Santé et la Protection '
-                  'Sociale à la Préfecture de'
-                  ' Meknès et par Délégation Ministérielle.\n'
-                    ' * Vu les dispositions du Dahir du 24 Février 1958 '
-                    'N° 1.58.008 article 40 portant réglementation sur les '
-                    'congés du personnel des Administrations Publiques, '
-                    'comme il a été modifié et complété.\n'
-                    ' * Vu les dispositions des lettres Ministérielle '
-                    'N° 5708 du 13/03/2020 et N° 10305 du 02/06/2021 '
-                    'relatives aux congés administratifs durant '
-                    'la pandémie (COVID-19)\n'
-                    ' * Vu la demande de congé présente par Mr(Mme) : ${nom}\n'
-                    " * Considérant les Droits à congé de l'intéressé(e) au titre de l'année: ${annee}",
-                style: pw.TextStyle(fontSize: 16),
+            pw.Text(
+              "lorem",
+              style: pw.TextStyle(fontSize: 16),
             ),
             pw.Align(
               alignment: pw.Alignment.center,
@@ -173,7 +127,7 @@ async{
 
             pw.RichText(
               text: pw.TextSpan(
-                text: "   *Au titre de l'année          ",
+                text: "            ",
                 style: pw.TextStyle(
                   fontSize: 16,
                   fontWeight: pw.FontWeight.bold,
@@ -193,7 +147,7 @@ async{
 
             pw.RichText(
               text: pw.TextSpan(
-                text: "   *Est accordé à Mr(Mme) ",
+                text: "   *Est accord(Mme) ",
                 style: pw.TextStyle(
                   fontSize: 16,
                   fontWeight: pw.FontWeight.bold,
@@ -213,7 +167,7 @@ async{
 
             pw.RichText(
               text: pw.TextSpan(
-                text: "   *PPR                                 ",
+                text: "   *jjkpk                               ",
                 style: pw.TextStyle(
                   fontSize: 16,
                   fontWeight: pw.FontWeight.bold,
@@ -253,7 +207,7 @@ async{
 
             pw.RichText(
               text: pw.TextSpan(
-                text: "   *En Fonction                    ",
+                text: "   *En Fonct                ",
                 style: pw.TextStyle(
                   fontSize: 16,
                   fontWeight: pw.FontWeight.bold,
@@ -273,7 +227,7 @@ async{
 
             pw.RichText(
               text: pw.TextSpan(
-                text: "   *A compter du                 ",
+                text: "   *A co du                 ",
                 style: pw.TextStyle(
                   fontSize: 16,
                   fontWeight: pw.FontWeight.bold,
@@ -301,7 +255,7 @@ async{
                 ),
                 children: [
                   pw.TextSpan(
-                    text: "-L'intérim sera assuré par ",
+                    text: "-L'inra as par ",
                     style: pw.TextStyle(
                       fontSize: 12,
                       fontWeight: pw.FontWeight.bold,
@@ -309,7 +263,7 @@ async{
                   ),
                   pw.TextSpan(
                     text: "\n                                             "
-                        "-En cas de nécessité de service le congé sera suspendu.",
+                        "-En cas de nécice le congé sera suspendu.",
                     style: pw.TextStyle(
                       fontSize: 12,
                       fontWeight: pw.FontWeight.bold,
@@ -317,7 +271,7 @@ async{
                   ),
                   pw.TextSpan(
                     text: "\n                                                 "
-                        "-L'intéressée doit être joignable durant la période du congé.",
+                        "-L'intnt la période du congé.",
                     style: pw.TextStyle(
                       fontSize: 12,
                       fontWeight: pw.FontWeight.bold,
@@ -325,9 +279,9 @@ async{
                   ),
                   pw.TextSpan(
                     text: "\n                                         "
-                        "LE DELEGUE DU MINISTERE DE LA SANTE\n"
+                        "LEE\n"
                         "                   "
-                        "ET LA PROTECTION SOCIALE A LA PREFECTURE DE MEKNES",
+                        "E",
                     style: pw.TextStyle(
                       fontSize: 15,
                       decoration: pw.TextDecoration.underline,
@@ -338,19 +292,19 @@ async{
               ),
             ),
             pw.RichText(
-                text: pw.TextSpan(
-                  text: "\n   Ampliations:\n\n",
-                  style: pw.TextStyle(
-                    fontSize: 17,
-                    decoration: pw.TextDecoration.underline,
-                    fontWeight: pw.FontWeight.normal,
-                    //color: PdfColors.black,
-                  ),
+              text: pw.TextSpan(
+                text: "\n   Ampliations:\n\n",
+                style: pw.TextStyle(
+                  fontSize: 17,
+                  decoration: pw.TextDecoration.underline,
+                  fontWeight: pw.FontWeight.normal,
+                  //color: PdfColors.black,
+                ),
               ),
             ),
             pw.RichText(
               text: pw.TextSpan(
-                text: "   *Mr le chef de SRES",
+                text: "   *M",
                 style: pw.TextStyle(
                   fontSize: 17,
                   fontWeight: pw.FontWeight.normal,
@@ -360,7 +314,7 @@ async{
             ),
             pw.RichText(
               text: pw.TextSpan(
-                text: "   *L'intéressé(e)(V.H)",
+                text: "   *L'in)",
                 style: pw.TextStyle(
                   fontSize: 17,
                   fontWeight: pw.FontWeight.normal,
@@ -370,7 +324,7 @@ async{
             ),
             pw.RichText(
               text: pw.TextSpan(
-                text: "   *Dossier",
+                text: "   *Doser",
                 style: pw.TextStyle(
                   fontSize: 17,
                   fontWeight: pw.FontWeight.normal,
@@ -417,5 +371,4 @@ void showPrintedToast(final BuildContext context){
 void showSharedToast(final BuildContext context){
   ScaffoldMessenger.of(context).showSnackBar(const SnackBar(content: Text("Document est bien partager")));
 }
-
 
